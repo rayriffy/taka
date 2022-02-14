35d31b6cd474ed3b769739fafbe807c7ab58c08e
Phumrapee Limpianchop <git@rayriffy.com>
2022-02-15 06:26:14 +0900

    Initial Commit

---
+++ タカ/__App.swift
@@ -0,0 +1,17 @@
+//
+//  __App.swift
+//  タカ
+//
+//  Created by rayriffy on 2022/02/15.
+//
+
+import SwiftUI
+
+@main
+struct __App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
